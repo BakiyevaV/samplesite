a80a520adf88be8f2ee0fdf07a548d9de481ea92
Виктория Бакиева <@itstep.loc>
2023-11-14 21:46:26 +0600

    14Nov files

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
-from bboard.models import Bb
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import BbForm
+from .models import Bb, Rubric
 from django.template import loader
 from django.shortcuts import render
 
 def index(request):
     bbs = Bb.objects.order_by('-publiched')
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all
+    context = {'bbs': bbs, 'rubrics': rubrics}
     return render( request,'index.html',context)
 
 
@@ -15,4 +20,22 @@ def index(request):
 #     context = {'bbs': bbs}
 #     return HttpResponse(template.render(context,request))
 
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric':current_rubric}
+    return render(request, 'by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
