6a23aeb2ebd8b3e25f57883ee719a29b6809452f
BakiyevaV <133982174+BakiyevaV@users.noreply.github.com>
2023-11-28 21:19:24 +0600

    28Nov files

--- bboard/models.py
+++ bboard/models.py
@@ -1,11 +1,27 @@
 import uuid
 
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
 
 is_all_posts_passive = False
 
 def is_active():
     return is_all_posts_passive
+
+def validate_even(val):
+    if val % 2 !=0:
+        raise ValidationError('Число %(value)s нечетное', code='odd', params={'value':val})
+
+class MinMaxValueValidator:
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self,val):
+        if val < self.min_value or val > self.max_value:
+            raise ValidationError('Число %(value)s должно находиться в диапазоне от: %(min)s до: %(max)s ',
+                                  code='out_of_range', params={'value':val, 'min': self.min_value, 'max': self.max_value})
 
 
 class Rubric(models.Model):
@@ -15,10 +31,21 @@ class Rubric(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs): #вызывается в момент сохранения данных в базу данных
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs): #вызывается в момент удаления данных из базы данных
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return f"/{self.pk}/"
+
     class Meta:
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
         ordering = ['name']
+
+
 
 
 class Bb(models.Model):
@@ -57,9 +84,9 @@ class Bb(models.Model):
                                #"Rubric" в ковычках, позволяет обращаться в любом порядке, раньше или позже кода строки без ковычек, значик модель должна быть реализована выше
                                verbose_name='Рубрика') # +helptext = '' помощь/описание без настройки не отображается
 
-    title = models.CharField(max_length=50, verbose_name="Товар")
+    title = models.CharField(max_length=50, verbose_name="Товар",validators=[validators.RegexValidator(regex='^.{4,}$')], error_messages= {'invalid':'Зачем вводишь некорректные данные?'})
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.DecimalField(verbose_name="Цена",default=0, max_digits=8, decimal_places=2)# +default= 0.0 дефолтное значение #
+    price = models.DecimalField(verbose_name="Цена",default=0, max_digits=8, decimal_places=2, validators=[validate_even,MinMaxValueValidator(25, 45)])# +default= 0.0 дефолтное значение #
     is_active = models.BooleanField(default=is_all_posts_passive)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")#auto_now_add - заполнение поля текущим временем в момент создания не изменяется
     updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Опубликовано")#auto_now - заполнение поля текущим временем в момент создания обновляемое
@@ -82,6 +109,31 @@ class Bb(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+    """def clean(self):
+        errors = {}
+        if not self.content:
+            errors['content'] = ValidationError('Укажите описание товара')
+        if self.price and self.price < 0:
+            errors['content'] = ValidationError('Цена должна быть больше нуля')
+        if errors:
+            raise ValidationError(errors)"""
+
+    def clean_title(self):
+        print("Не продаем бобров!")
+        errors = {}
+        if self.title == 'Бобёр':
+            print("Проверка наименования")
+            errors['content'] = ValidationError('Бобры не продаются')
+            raise ValidationError(errors)
+
+
+    def title_and_price (self):
+        if self.price:
+            return  f"{self.title}{self.price:.2f}"
+        return self.title
+
+    title_and_price.short_description = 'Название и цена' #только для админки
 
 
     class Meta:
