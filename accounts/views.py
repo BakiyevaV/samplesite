c28e768cd539a0446a4c16249a3b1adcb2d53504
BakiyevaV <133982174+BakiyevaV@users.noreply.github.com>
2024-01-14 16:49:09 +0600

    Added hw for 21Dec

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
+import json
+import datetime
+
+from django.http import HttpHeaders
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
 from .models import Clients
 from .forms import UserForm, LoginForm
@@ -9,11 +13,9 @@ class UserCreateView(CreateView):
     template_name = 'registration.html'
     form_class = UserForm
     model = Clients
-    success_url = reverse_lazy('index')
 def login_view(request):
-
-    a = Clients.objects.all()
     if request.method == 'POST':
+        request_write(request)
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['login']
@@ -24,10 +26,54 @@ def login_view(request):
                 if u.login == username and u.password == password:
                     user = u
             if user is not None:
-                return redirect('index')  # Replace 'main_page' with the actual URL name for your main page
+                return redirect(reverse('bboard:index'))
+            else:
+                return redirect(reverse('registration'))
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+def request_write(request):
+    data = {}
+    request_params = ('scheme', 'body', 'path', 'method', 'encoding', 'content_type', 'GET',
+                      'POST', 'COOKIES', 'FILES', 'headers',
+                      {'META': ('CONTENT_LENGTH', 'CONTENT_TYPE', 'HTTP_ACCEPT', 'HTTP_HOST', 'HTTP_REFERER',
+                                'HTTP_USER_AGENT', 'QUERY_STRING', 'REMOTE_ADDR')})
+    with open('logs.json', 'w', encoding='utf-8') as log_file:
+        print(request.META["HTTP_HOST"])
+        for param in request_params:
+            if type(param) == str:
+                data[param] = decoder(getattr(request, param))
+            else:
+                m_key = ""
+                meta_p = {}
+                for key, subparams in param.items():
+                    m_key = key
+                    obj = getattr(request, key)
+                    for subparam in subparams:
+                        meta_p[f'{subparam}'] = decoder(obj[f'{subparam}'])
+
+                data[m_key] = meta_p
+        data_for_write = {f"{datetime.datetime.now()}":data}
+        json.dump(data_for_write, log_file)
+
+def decoder(value):
+    if isinstance(value, bytes):
+        return value.decode("utf-8")
+    elif isinstance(value, HttpHeaders):
+        return dict(value)
+    return value
+
+
+
+
+
+
+
+
+
+
+
 
 
 
